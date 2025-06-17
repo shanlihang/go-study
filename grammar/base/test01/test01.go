5aa0b7bf02a8dd9641dbf2536a3134b8b4c2c3d5
shanlihang <67490009@qq.com>
2025-06-17 22:31:42 +0800

    feat：测试init函数调用时机及次数

---
+++ grammar/base/test01/test01.go
@@ -0,0 +1,7 @@
+package test01
+
+import "fmt"
+
+func init() {
+	fmt.Println("init函数执行了")
+}
---
+++ grammar/base/test02/test.go
@@ -0,0 +1,10 @@
+package test02
+
+import (
+	"fmt"
+	_ "grammar/base/test01"
+)
+
+func Add() {
+	fmt.Println("123456")
+}
